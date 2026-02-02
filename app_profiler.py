0654e9eaaaa2656f2e502378fc22eeca131e2a2a
DavidKhumalo2026 <david.khumalo@gmail.com>
2026-02-02 19:48:21 +0200

    Update app_profiler.py

--- app_profiler.py
+++ app_profiler.py
@@ -23,13 +23,13 @@ st.image(
 
 # Researcher qualifications, interest, and affiliation
 Qualifications = "BSc(Hons),MBA,MSc(Med)"
-Research = "Physical activity epidemiology,Exerkines,Metabolic syndrome"
+Interest = "Physical activity epidemiology,Exerkines,Metabolic syndrome"
 Affiliation = "South Africa Sports Medicine Association (SASMA)"
 
 # Display basic profile information
 st.header("Researcher Qualification, Interest, and Affiliation")
 st.write(f"**Qualifications:** {Qualifications}")
-st.write(f"**Research:** {Interests}")
+st.write(f"**Interest:** {Interests}")
 st.write(f"**Affiliation:** {Affiliation}")
 
 # Add a section for publications
@@ -130,6 +130,7 @@ st.header("Contact Information")
 email = "david.khumalo@gmail.com"
 
 st.write(f"You can reach {name} at {email}.")
+
 
 
 
