95ec5e6bb33fc73a9a169b571364364f921d74fc
DavidKhumalo2026 <david.khumalo@gmail.com>
2026-02-02 19:50:19 +0200

    Update app_profiler.py

--- app_profiler.py
+++ app_profiler.py
@@ -29,7 +29,7 @@ Affiliation = "South Africa Sports Medicine Association (SASMA)"
 # Display basic profile information
 st.header("Researcher Qualification, Interest, and Affiliation")
 st.write(f"**Qualifications:** {Qualifications}")
-st.write(f"**Interest:** {Interests}")
+st.write(f"**Interest:** {Interest}")
 st.write(f"**Affiliation:** {Affiliation}")
 
 # Add a section for publications
@@ -130,6 +130,7 @@ st.header("Contact Information")
 email = "david.khumalo@gmail.com"
 
 st.write(f"You can reach {name} at {email}.")
+
 
 
 
