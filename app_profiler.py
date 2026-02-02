72e9be3d2bceb7a35494b0255fd2a522cb786355
DavidKhumalo2026 <david.khumalo@gmail.com>
2026-02-02 14:22:18 +0200

    Update app_profiler.py

--- app_profiler.py
+++ app_profiler.py
@@ -6,9 +6,9 @@ import numpy as np
 st.title("Researcher Profile Page with STEM Data")
 
 # Collect basic information
-name = "Dr. Jane Doe"
-field = "Astrophysics"
-institution = "University of Science"
+name = "David Vusumuzi Khumalo"
+field = "Sport and Exercise Science"
+institution = "University of the Witwatersrand"
 
 # Display basic profile information
 st.header("Researcher Overview")
@@ -117,4 +117,5 @@ elif data_option == "Weather Data":
 # Add a contact section
 st.header("Contact Information")
 email = "jane.doe@example.com"
+
 st.write(f"You can reach {name} at {email}.")
